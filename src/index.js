240d5b6ef0971a73512dc9acf1a92f4a66266747
Steven Beeching <steven.beeching@gmail.com>
2021-06-15 10:35:17 +0100

    Turn off service worker refresh page

--- src/index.js
+++ src/index.js
@@ -11,10 +11,15 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+setInterval(() => {
+  console.log("rloadings");
+  window.location.reload();
+}, 21600000);
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
